a367c0177c4f597f43285c01579a7e7b5b3e14bc
F1reMonster <62647213+F1reMonster@users.noreply.github.com>
2023-04-25 21:48:18 +0300

    main page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
 module.exports = {
-	mode: "jit", // Just-In-Time Compiler
+	// mode: "jit", // Just-In-Time Compiler
 	content: ["./src/**/*.html"],
 	darkMode: "media", // or 'media' or 'class'
 	theme: {
 		screens: {
 			lg: { max: "992px" },
 			md: { max: "768px" },
+			mdd: { max: "575px" },
 			sm: { max: "480px" },
+			smm: { max: "400px" },
 			lw: { max: "374px" },
 		},
 		container: {
@@ -14,19 +16,30 @@ module.exports = {
 			center: true,
 		},
 		extend: {
+			screens: {
+				// max-height
+				"mx-h884": { raw: "(max-height: 884px)" },
+				"mx-h860": { raw: "(max-height: 860px)" },
+				"mx-h718": { raw: "(max-height: 718px)" },
+				"mx-h717": { raw: "(max-height: 717px)" },
+				"mx-h648": { raw: "(max-height: 648px)" },
+				"mx-h550": { raw: "(max-height: 550px)" },
+				"mx-h450": { raw: "(max-height: 450px)" },
+				// min-height
+				"mn-h992": { raw: "(min-height: 992px)" },
+			},
+			boxShadow: {
+				custom: "inset 0px 4px 5px rgba(65, 99, 207, 0.29)",
+			},
 			colors: {
-				"charade": "#232532",
-				"indigo": "#4163CF",
-				"perano": "#BDCBF6",
-
+				charade: "#232532",
+				indigo: "#4163CF",
+				perano: "#BDCBF6",
 			},
 		},
 	},
 	variants: {
 		extend: {},
 	},
-	plugins: [
-		require("@tailwindcss/forms"),
-		require('prettier-plugin-tailwindcss')
-	],
+	plugins: [require("@tailwindcss/forms"), require("prettier-plugin-tailwindcss")],
 };
